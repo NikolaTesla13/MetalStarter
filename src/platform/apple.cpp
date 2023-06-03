be007760e40d046e4bffdf7fa9f0450067614d1c
Asandei Stefan <asandei.stefanel@gmail.com>
2023-06-03 15:00:09 +0300

    first commit

---
+++ src/platform/apple.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Stefan on 6/3/2023.
+//
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+
+#include "apple.h"
---
+++ src/platform/apple.h
@@ -0,0 +1,12 @@
+//
+// Created by Stefan on 6/3/2023.
+//
+
+#ifndef METALSTARTER_APPLE_H
+#define METALSTARTER_APPLE_H
+
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
+
+#endif //METALSTARTER_APPLE_H
